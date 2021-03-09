43f4634f88abed3ed4b7f32e862222e834c7ca12
diegogmach@outlook.com <diegogmach@outlook.com>
2021-03-09 18:28:36 -0300

    Creating Entities

---
+++ src/main/java/com/agendamentodeconsulta/entity/Address.java
@@ -0,0 +1,18 @@
+package com.agendamentodeconsulta.entity;
+
+public class Address {
+
+    private Long id;
+
+    private String street;
+
+    private String district;
+
+    private String number;
+
+    private String city;
+
+    private String federativeUnion;
+
+}
+
